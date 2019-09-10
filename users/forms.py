0f72b6564207742d07fc7d95544acff389007dee
NinjaAiden <aidenharvey102@gmail.com>
2019-09-10 17:24:46 +0000

    refactored users app for improved usability

--- users/forms.py
+++ users/forms.py
@@ -1,15 +1,42 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-class CustomUserCreationForm(UserCreationForm):
+class UserLoginForm(forms.Form):
+    """Form to be used to login users"""
 
-    class Meta(UserCreationForm):
-        model = CustomUser
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
-class CustomUserChangeForm(UserChangeForm):
+class UserRegistrationForm(UserCreationForm):
+    """Form used to register user"""
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",
+        widget=forms.PasswordInput)
 
     class Meta:
-        model = CustomUser
-        fields = ('first_name', 'last_name', 'username', 'email',)
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(email=email).exclude(username=username):
+            raise forms.ValidationError(u"Email address must be unique")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if not password1 or not password2: # pragma: no cover
+            raise forms.ValidationError(u"Please confirm your password")
+
+        if password1 != password2:
+            raise forms.ValidationError(u"Password must match")
+
+        return password2
--- users/models.py
+++ users/models.py
@@ -5,8 +5,3 @@ class CustomUser(AbstractUser):
     email = models.EmailField(('email address'), unique=True)
     first_name = models.CharField(('first name'), max_length=30, blank=True)
     last_name = models.CharField(('last name'), max_length=30, blank=True)
-    user_name = models.CharField(('username'), max_length=40, blank=True)
-    date_joined = models.DateTimeField(('date joined'), auto_now_add=True)
-    amount_donated = models.PositiveIntegerField(default=0)
-    number_of_posts = models.PositiveIntegerField(default=0)
-    status = models.CharField(('status'), max_length=20, default='Member')
